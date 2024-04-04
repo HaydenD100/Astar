95fb298423b03b0121d0039e7bbab8668e5dd3ca
Hayden <112908824+HaydenD100@users.noreply.github.com>
2024-04-04 17:34:24 -0400

    Update main.cpp
    Algorithm has been updated to show the shortest path, this is shown as dark red

--- main.cpp
+++ main.cpp
@@ -15,29 +15,47 @@ Press any button on the keyboard to start finding the path
 
 */
 
+//Window size
 const int WIDTH = 800, HEIGHT = 600;
 
+//Grid Structure this holds all the information on each point
 struct GridPoint {
+	//Cordinates 
 	int x;
 	int y;
+	//Bool for if the point is solid etc a wall 0 for false 1 for true, solid points are shown in gray
 	int solid;
+	//Bool for if the algorithm has picked this point as the best point this is shown in red
 	int pickedPoint;
+	//Bool for if this point has already been calculated, this is used so there arent repated calculated points in the calculated vectors, tiles that have been calculated but not picked are shown in blue
 	int calculated;
+	//Bool to check if this tile has been picked as the shortest path this is shown as dark red
+	int truePath = 0;
+	
+	//H cost is calculated by how far this point is from the end point
 	int hCost;
+	//G cost is calculated by how far this point is from the start point
 	int gCost;
+	//F = gCost + hCost
 	int fCost;
+	//the last node that was chosen as the best node
+	GridPoint* parent;
+
 
 };
 
+//Starting and ending point
 const int startingX = 20; 
 const int startingY = 20;
 const int endX = 60; 
 const int endY = 50;
 
+//2D Grid of all the points
 struct GridPoint grid[80][60];
+//This vector stores all Calculated points
 std::vector<GridPoint*> CalculatedPoints;
 
-
+//This function draws all the points in grid and assigns each tile to a certain colour depending on if it is a picked point, solid point, calculated point or end/starting point
 void Draw(SDL_Renderer* renderer) {
 	//sets the background colour and then clears the window 
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
@@ -47,23 +65,25 @@ void Draw(SDL_Renderer* renderer) {
 		for (int x = 0; x < 80; x++) {
 			SDL_Rect rect = { x * 10,y * 10,9,9 };
 
-			if (grid[x][y].pickedPoint == 1) {
-				SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-			}
-			else if(x == startingX && y == startingY)
+			
+			if(x == startingX && y == startingY)
 			{
 				SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-
+			}
+			else if (x == endX && y == endY) {
+				SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
+			}
+			else if (grid[x][y].truePath == 1) {
+				SDL_SetRenderDrawColor(renderer, 128, 0, 0, 255);
+			}
+			else if (grid[x][y].pickedPoint == 1) {
+				SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 			}
 			else if (grid[x][y].solid == 1) {
 				SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
-
 			}
 			else if (grid[x][y].calculated == 1 && grid[x][y].pickedPoint == 0) {
 				SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-			}
-			else if(x == endX && y == endY){
-				SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
 			}
 			else {
 				SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
@@ -77,46 +97,68 @@ void Draw(SDL_Renderer* renderer) {
 
 }
 
-void CalculateCost(struct GridPoint *point) {
+//This function calculates the H, G and F cost of each poiunt
+void CalculateCost(struct GridPoint *point,int x,int y) {
+	if (point->solid == 1)
+		return;
 	
-	point->gCost = std::sqrt((startingX - point->x) * (startingX - point->x) + (startingY - point->y) * (startingY - point->y));
-	point->hCost = std::sqrt((endX - point->x) * (endX - point->x) + (endY - point->y) * (endY - point->y));
+	point->gCost = std::sqrt((startingX - point->x) * (startingX - point->x) + (startingY - point->y) * (startingY - point->y)) * 10;
+	point->hCost = std::sqrt((endX - point->x) * (endX - point->x) + (endY - point->y) * (endY - point->y)) * 10;
 	point->fCost = point->hCost + point->gCost;
-	if (point->solid == 0 && point->pickedPoint == 0 && point->calculated == 0)
+	//This changes the parent point for any calculated node to its most recently picked neighbour.
+	if (point->pickedPoint == 0) {
+		point->parent = &grid[x][y];
+	}
+	//If the point is newly calculated add the point to the CalculatedPoints vector
+	if (point->pickedPoint == 0 && point->calculated == 0)
 	{
 		point->calculated = 1;
 		CalculatedPoints.push_back(point);
 	}
-		
-	
 }
 
+//This function runs after the end point has been found and finds the shortest path between the start and end by going through all the parentpoints starting from the end point
+//This function can be modified to store the parent points in a vector giving a vector of all points someone would have to follow to go from the start to the end
+void FindPath() {
+	GridPoint* currentTIle = &grid[endX][endY];
+
+	while (currentTIle->x != startingX || currentTIle->y != startingY) {
+		currentTIle->truePath = 1;
+		currentTIle = currentTIle->parent;
+	}
+
+}
+
+
+
+//This function calculates all neighbouring points of the inputed point (x,y) and then selects the next point by checking the CalculatedPoints vector and choosing the point with the lowest F value, if there are multiple lowest F values then it picks the one with the Lowest H value
 struct GridPoint* NextPoint(int x, int y) {
 
 	//Calculate all the points next to current point
 	if (x > 1) {
-		CalculateCost(&grid[x - 1][y]);
+		CalculateCost(&grid[x - 1][y],x,y);
+		
 	}
 	if (x < 79) {
-		CalculateCost(&grid[x + 1][y]);
+		CalculateCost(&grid[x + 1][y], x, y);
 	}
 	if (x > 1 && y > 1) {
-		CalculateCost(&grid[x - 1][y - 1]);
+		CalculateCost(&grid[x - 1][y - 1], x, y);
 	}
 	if (x > 1 && y > 59) {
-		CalculateCost(&grid[x - 1][y + 1]);
+		CalculateCost(&grid[x - 1][y + 1], x, y);
 	}
 	if (x < 79 && y > 1) {
-		CalculateCost(&grid[x + 1][y - 1]);
+		CalculateCost(&grid[x + 1][y - 1], x, y);
 	}
 	if (x < 79 && y < 59) {
-		CalculateCost(&grid[x + 1][y + 1]);
+		CalculateCost(&grid[x + 1][y + 1], x, y);
 	}
 	if (y > 1) {
-		CalculateCost(&grid[x][y - 1]);
+		CalculateCost(&grid[x][y - 1], x, y);
 	}
 	if (y < 79) {
-		CalculateCost(&grid[x][y + 1]);
+		CalculateCost(&grid[x][y + 1], x, y);
 	}
 
 	GridPoint* lowestCostPoint = CalculatedPoints[0];
@@ -137,10 +179,9 @@ struct GridPoint* NextPoint(int x, int y) {
 	CalculatedPoints.erase(std::find(CalculatedPoints.begin(),CalculatedPoints.end(),lowestCostPoint));
 	lowestCostPoint->pickedPoint = 1;
 
-	return lowestCostPoint;
 	
 
-
+	return lowestCostPoint;
 }
 
 
@@ -150,6 +191,8 @@ int main(int argc, char* args[]) {
 
 	int CurrentX = startingX;
 	int CurrentY = startingY;
+
+	//Mouse Pos
 	int mouseX;
 	int mouseY;
 
@@ -175,14 +218,13 @@ int main(int argc, char* args[]) {
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
 
-	while (1) {
+	while (true) {
 		if (SDL_PollEvent(&event)) {
 			if (SDL_QUIT == event.type) break;
 			if (SDL_GetMouseState(&mouseX, &mouseY) & SDL_BUTTON_LMASK) {
 				if (mouseX >= 0 && mouseX <= WIDTH && mouseY >= 0 && mouseY <= HEIGHT)
 				{
 					grid[mouseX / 10][mouseY / 10].solid = 1;
-
 				}
 					
 			}
@@ -200,6 +242,7 @@ int main(int argc, char* args[]) {
 			{
 				solve = 0;
 				std::cout << "solved";
+				FindPath();
 			}
 				
 		}
